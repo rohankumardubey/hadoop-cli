7f732bfe6bcedf11fe11a0ead38b245101f335d2
Stephen ODonnell <stephen.odonnell@gmail.com>
2015-06-08 21:57:57 +0100

    Changed the indent size to 2 spaces. Also implemented Configured and ToolRunner so that the HDFS configuration gets picked up automatically from the class path

--- src/main/java/com/appsintheopen/hadoopcli/Cli.java
+++ src/main/java/com/appsintheopen/hadoopcli/Cli.java
@@ -18,120 +18,120 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 import jline.console.ConsoleReader;
 import jline.console.completer.Completer;
 import jline.console.completer.FileNameCompleter;
 import jline.console.completer.StringsCompleter;
 
-public class Cli
+public class Cli extends Configured implements Tool
 {
-    public static void usage() {
-        System.out.println("Usage: java " + Cli.class.getName()
-            + " [none/simple/files/dictionary [trigger mask]]");
-        System.out.println("  none - no completors");
-        System.out.println("  simple - a simple completor that comples "
-            + "\"foo\", \"bar\", and \"baz\"");
-        System.out
-            .println("  files - a completor that comples " + "file names");
-        System.out.println("  classes - a completor that comples "
-            + "java class names");
-        System.out
-            .println("  trigger - a special word which causes it to assume "
-                + "the next line is a password");
-        System.out.println("  mask - is the character to print in place of "
-            + "the actual password character");
-        System.out.println("  color - colored prompt and feedback");
-        System.out.println("\n  E.g - java Example simple su '*'\n"
-            + "will use the simple compleator with 'su' triggering\n"
-            + "the use of '*' as a password mask.");
-    }
+  public static void usage() {
+    System.out.println("Usage: java " + Cli.class.getName()
+        + " [none/simple/files/dictionary [trigger mask]]");
+    System.out.println("  none - no completors");
+    System.out.println("  simple - a simple completor that comples "
+        + "\"foo\", \"bar\", and \"baz\"");
+    System.out.println("  files - a completor that comples " + "file names");
+    System.out.println("  classes - a completor that comples "
+        + "java class names");
+    System.out
+    .println("  trigger - a special word which causes it to assume "
+        + "the next line is a password");
+    System.out.println("  mask - is the character to print in place of "
+        + "the actual password character");
+    System.out.println("  color - colored prompt and feedback");
+    System.out.println("\n  E.g - java Example simple su '*'\n"
+        + "will use the simple compleator with 'su' triggering\n"
+        + "the use of '*' as a password mask.");
+  }
+    
+  public int run(String[] args) throws Exception {
+    Configuration conf = this.getConf();
 
-    public static void main(String[] args) throws IOException {
-        try {
-            Character mask = null;
-            String trigger = null;
-            boolean color = false;
+    try {
+      Character mask = null;
+      String trigger = null;
+      boolean color = false;
 
-            ConsoleReader reader = new ConsoleReader();
+      ConsoleReader reader = new ConsoleReader();
+      reader.setPrompt("prompt> ");
 
-            reader.setPrompt("prompt> ");
+      if ((args == null) || (args.length == 0)) {
+        usage();
+        return 0;
+      }
 
-            if ((args == null) || (args.length == 0)) {
-                usage();
-
-                return;
-            }
-
-            List<Completer> completors = new LinkedList<Completer>();
-            
-            
-            Configuration conf = new Configuration();
-            conf.addResource(new Path("/Users/sodonnel/source/puppet/core-site.xml"));
-            conf.addResource(new Path("/Users/sodonnel/source/puppet/hdfs-site.xml"));
-            conf.addResource(new Path("/Users/sodonnel/source/puppet/mapred-site.xml"));
-            
-
-            if (args.length > 0) {
-                if (args[0].equals("none")) {
-                }
-                else if (args[0].equals("files")) {
-                    completors.add(new FileNameCompleter());
-                }
-                else if (args[0].equals("hdfs")) {
-                    completors.add(new HdfsCompleter(conf));
-                }
-                else if (args[0].equals("simple")) {
-                    completors.add(new StringsCompleter("foo", "bar", "baz"));
-                }
-                else if (args[0].equals("color")) {
-                    color = true;
-                    reader.setPrompt("\u001B[1mfoo\u001B[0m@bar\u001B[32m@baz\u001B[0m> ");
-                }
-                else {
-                    usage();
-
-                    return;
-                }
-            }
-
-            if (args.length == 3) {
-                mask = args[2].charAt(0);
-                trigger = args[1];
-            }
-
-            for (Completer c : completors) {
-                reader.addCompleter(c);
-            }
-
-            String line;
-            PrintWriter out = new PrintWriter(reader.getOutput());
-
-            while ((line = reader.readLine()) != null) {
-                if (color){
-                    out.println("\u001B[33m======>\u001B[0m\"" + line + "\"");
-
-                } else {
-                    out.println("======>\"" + line + "\"");
-                }
-                out.flush();
-
-                // If we input the special word then we will mask
-                // the next line.
-                if ((trigger != null) && (line.compareTo(trigger) == 0)) {
-                    line = reader.readLine("password> ", mask);
-                }
-                if (line.equalsIgnoreCase("quit") || line.equalsIgnoreCase("exit")) {
-                    break;
-                }
-                if (line.equalsIgnoreCase("cls")) {
-                    reader.clearScreen();
-                }
-            }
+      List<Completer> completors = new LinkedList<Completer>();
+      
+      if (args.length > 0) {
+        if (args[0].equals("none")) {
         }
-        catch (Throwable t) {
-            t.printStackTrace();
+        else if (args[0].equals("files")) {
+          completors.add(new FileNameCompleter());
         }
+        else if (args[0].equals("hdfs")) {
+          completors.add(new HdfsCompleter(conf));
+        }
+        else if (args[0].equals("simple")) {
+          completors.add(new StringsCompleter("foo", "bar", "baz"));
+        }
+        else if (args[0].equals("color")) {
+          color = true;
+          reader.setPrompt("\u001B[1mfoo\u001B[0m@bar\u001B[32m@baz\u001B[0m> ");
+        }
+        else {
+          usage();
+          return 0;
+        }
+      }
+
+      if (args.length == 3) {
+        mask = args[2].charAt(0);
+        trigger = args[1];
+      }
+      
+      for (Completer c : completors) {
+        reader.addCompleter(c);
+      }
+
+      String line;
+      PrintWriter out = new PrintWriter(reader.getOutput());
+
+      while ((line = reader.readLine()) != null) {
+        if (color){
+          out.println("\u001B[33m======>\u001B[0m\"" + line + "\"");
+          
+        } else {
+          out.println("======>\"" + line + "\"");
+        }
+        out.flush();
+        
+        // If we input the special word then we will mask
+        // the next line.
+        if ((trigger != null) && (line.compareTo(trigger) == 0)) {
+          line = reader.readLine("password> ", mask);
+        }
+        if (line.equalsIgnoreCase("quit") || line.equalsIgnoreCase("exit")) {
+          break;
+        }
+        if (line.equalsIgnoreCase("cls")) {
+          reader.clearScreen();
+        }
+      }
     }
+    catch (Throwable t) {
+      t.printStackTrace();
+      return -1;
+    }
+    return 0;
+  }
+      
+  public static void main(String[] args) throws Exception {
+    ToolRunner.run(new Cli(), args);
+  }
 }
--- src/main/java/com/appsintheopen/hadoopcli/HdfsCompleter.java
+++ src/main/java/com/appsintheopen/hadoopcli/HdfsCompleter.java
@@ -8,95 +8,96 @@ import java.util.List;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileUtil;
 import org.apache.hadoop.fs.Path;
 
 import jline.console.completer.Completer;
 
-public class HdfsCompleter 
-    implements Completer {
+public class HdfsCompleter implements Completer {
     
-    private FileSystem fs;
-    private String fsScheme; 
+  private FileSystem fs;
+  private String fsScheme; 
     
-    public HdfsCompleter(Configuration conf) throws IOException {
-        fs = FileSystem.get(conf);
-        fsScheme = fs.getUri().toString();
+  public HdfsCompleter(Configuration conf) throws IOException {
+    fs = FileSystem.get(conf);
+    fsScheme = fs.getUri().toString();
+  }
+
+  @Override
+  public int complete(String buffer, int cursor, List<CharSequence> candidates) {
+    // Treat a null buffer as empty string
+    if (buffer == null) {
+      buffer = "";
+    }
+        
+    FileStatus[] files;
+    try {
+      files = getFiles(buffer);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+      return -1;
+    }
+        
+    if (files.length == 0) {
+      return -1;
+    }
+        
+    int matches = 0;
+    
+    for (int i = 0; i < files.length; i++) {
+      if (removeScheme(files[i].getPath().toString()).startsWith(buffer)) {
+        matches ++;
+      }
     }
 
-    @Override
-    public int complete(String buffer, int cursor, List<CharSequence> candidates) {
-        // Treat a null buffer as empty string
-        if (buffer == null) {
-            buffer = "";
-        }
-        
-        FileStatus[] files;
-        try {
-            files = getFiles(buffer);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return -1;
-        }
-        
-        if (files.length == 0) {
-            return -1;
-        }
-        
-        int matches = 0;
-        
-        for (int i = 0; i < files.length; i++) {
-            if (removeScheme(files[i].getPath().toString()).startsWith(buffer)) {
-                matches ++;
-            }
-        }
-
-        for (int i = 0; i < files.length; i++) {
-            String pathName = removeScheme(files[i].getPath().toString());
-            if (pathName.startsWith(buffer)) {
-                CharSequence name = files[i].getPath().getName() + (matches == 1 && files[i].isDirectory() ? "/" : " ");
-                candidates.add(name);
-            }
-        }
-
-        final int index = buffer.lastIndexOf("/");
-
-        return index + 1; //separator().length();
-        
+    for (int i = 0; i < files.length; i++) {
+      String pathName = removeScheme(files[i].getPath().toString());
+      if (pathName.startsWith(buffer)) {
+        CharSequence name = files[i].getPath().getName() + (matches == 1 && files[i].isDirectory() ? "/" : " ");
+        candidates.add(name);
+      }
     }
+
+    final int index = buffer.lastIndexOf("/");
     
-    public FileStatus[] getFiles(String path) throws IOException {
+    return index + 1; //separator().length();
         
-        Path   directory;
-        Path thePath = path.isEmpty() ? fs.getHomeDirectory() : new Path(path );
-        
-        if (thePath.isRoot()) {
-            directory = thePath;
-        } else {
-            // If the original path string has a trailing "/" then we know it was already
-            // a directory. When the string is turned into a "path" the trailing / is lost.
-            // This is why the following statement users both thePath and path :-/
-            if (fs.isDirectory(thePath) && path.endsWith("/")) {
-                directory = thePath;
-            } else {
-                directory = thePath.getParent();
-            }
-        }
-        
-        try {
-            if (fs.exists(directory)) {
-                return fs.listStatus(directory);
-            } else {
-                return new FileStatus[0];
-            }
-        } catch (org.apache.hadoop.security.AccessControlException e) {
-            // If you don't have permission, then the file 'doesn't exist'
-            return new FileStatus[0];
-        }
-   }
+  }
     
-    private String removeScheme(String path) {
-        return path.replace(fsScheme, "");
+  public FileStatus[] getFiles(String path) throws IOException {
+        
+    Path   directory;
+    Path thePath = path.isEmpty() ? fs.getHomeDirectory() : new Path(path );
+    
+    if (thePath.isRoot()) {
+      directory = thePath;
+    } else {
+      // If the original path string has a trailing "/" then we know it was already
+      // a directory. When the string is turned into a "path" the trailing / is lost.
+      // This is why the following statement users both thePath and path :-/
+      if (fs.isDirectory(thePath) && path.endsWith("/")) {
+        directory = thePath;
+      } else {
+        directory = thePath.getParent();
+      }
     }
+        
+    try {
+      if (fs.exists(directory)) {
+        //Path[] paths = FileUtil.stat2Paths(fs.listStatus(directory));
+        return fs.listStatus(directory);
+      } else {
+        return new FileStatus[0];
+      }
+    } catch (org.apache.hadoop.security.AccessControlException e) {
+      // If you don't have permission, then the file 'doesn't exist'
+      return new FileStatus[0];
+    }
+  }
+    
+  private String removeScheme(String path) {
+    return path.replace(fsScheme, "");
+  }
 
 }
